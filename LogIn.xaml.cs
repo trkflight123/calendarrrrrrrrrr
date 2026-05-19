0292ad300e565f34e854ac2fb6e896eda0aa13fb
perolameowa <s.saraga.535933@umindanao.edu.ph>
2026-05-20 01:28:11 +0800

    apil na login page fo.

---
+++ LogIn.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace calendarrrrrrrrrr
+{
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void TopBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            Close();
+        }
+    }
+}
